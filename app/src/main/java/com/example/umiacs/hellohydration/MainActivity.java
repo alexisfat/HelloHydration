50a776407c5fe758e3858dd2fff5b32c4e406e57
nbehrje <nbehrje@gmail.com>
2016-11-30 21:35:22 -0500

    Adds toolbar to MainActivity and Settings activity

--- app/src/main/java/com/example/umiacs/hellohydration/MainActivity.java
+++ app/src/main/java/com/example/umiacs/hellohydration/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.umiacs.hellohydration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    //sets up toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //handles selected toolbar menu option
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this, Settings.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
