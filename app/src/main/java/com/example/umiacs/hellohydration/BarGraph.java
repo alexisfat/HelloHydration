9097f910a46aa162b9df728eae66fc75d7c51774
Alex Berlinger <alexberlinger@aol.com>
2016-12-11 11:29:22 -0500

    bars now clickable and goes to activity tracker for that day

--- app/src/main/java/com/example/umiacs/hellohydration/BarGraph.java
+++ app/src/main/java/com/example/umiacs/hellohydration/BarGraph.java
@@ -1,16 +1,13 @@
 package com.example.umiacs.hellohydration;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
 
@@ -23,15 +20,8 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.ChartTouchListener;
-import com.github.mikephil.charting.listener.OnChartGestureListener;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 
@@ -55,8 +45,6 @@ public class BarGraph extends AppCompatActivity /*implements OnChartValueSelecte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bargraph);
-        findViewById(R.id.bargraph).setBackgroundColor(Color.parseColor("#E0FCFC"));
-
         /*Create the bar chart
          * If no data is recorded this should say "Start drinking!"
          */
@@ -77,7 +65,9 @@ public class BarGraph extends AppCompatActivity /*implements OnChartValueSelecte
         /*This created the data set to be plotted,
         * if legend was showed this would display water consumption*/
         BarDataSet barDataSet = new BarDataSet(barEntries, "Water Consumption");
+        barDataSet.setHighlightEnabled(true);
         BarData barData = new BarData(barDataSet);
+
         /*Makes all the bars the same size*/
         barChart.setFitBars(true);
         barChart.setData(barData);
@@ -85,6 +75,7 @@ public class BarGraph extends AppCompatActivity /*implements OnChartValueSelecte
         /*Hides the legend because only displaying one measure*/
         Legend legend = barChart.getLegend();
         legend.setEnabled(false);
+        barChart.setHighlightFullBarEnabled(true);
 
         /*Allows users to pitch to zoom*/
         barChart.setScaleEnabled(true);
@@ -104,6 +95,8 @@ public class BarGraph extends AppCompatActivity /*implements OnChartValueSelecte
         xAxis.setDrawGridLines(false);
         xAxis.setAxisMinimum(0f);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.GRAY);
+        xAxis.setTextSize(12f);
 
         /*Y-Axis options
         * Hide the right side of the axis
@@ -116,13 +109,13 @@ public class BarGraph extends AppCompatActivity /*implements OnChartValueSelecte
         /*Add a limit line to designated reached goals
         * Options:
         *  Change text color to navy
-        *  Make text size 10
+        *  Make text size 14
         *  Make line color navy
         * */
         LimitLine goal = new LimitLine(10f, "Goal Reached!");
         goal.setLineWidth(3f);
         goal.setTextColor(Color.rgb(4,50,124));
-        goal.setTextSize(10f);
+        goal.setTextSize(14f);
         goal.setLineColor(Color.rgb(4,50,124));
         yLeftAxis.addLimitLine(goal);
 
@@ -130,31 +123,73 @@ public class BarGraph extends AppCompatActivity /*implements OnChartValueSelecte
         yLeftAxis.setDrawAxisLine(true);
         yLeftAxis.setLabelCount(10, true);
         yLeftAxis.setDrawGridLines(true); //  grid lines
+        yLeftAxis.setTextColor(Color.GRAY);
+        yLeftAxis.setTextSize(14f);
         //yRightAxis.setDrawGridLines(false); // no grid lines
         /*Take away description*/
         barChart.setDescription(null);
-        //set a listerned if bars are clicked
-        //barChart.setOnChartGestureListener(this);
+        //set a listener if bars are clicked
         barChart.setHighlightPerTapEnabled(true);
         barChart.setClickable(true);
-        barChart.setOnClickListener(new View.OnClickListener(){
+
+
+        /*Set values for exercise based on where click occurs,
+         * i.e. what bar/day of the week
+         * */
+        barChart.setOnTouchListener(new View.OnTouchListener(){
             @Override
-            public void onClick(View v) {
-                //if(barChart.va)
-                Log.d("TAG", "TOUCHED IN : " + v.getId());
-                setContentView(R.layout.activity_exercisetracker);
-               // Intent activityTracker = new Intent(BarGraph.this, ExerciseTracker.class);
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        setContentView(R.layout.activity_exercisetracker);
+                        TextView bikingTime = (TextView) findViewById(R.id.bikingTime);
+                        TextView walkingTime = (TextView) findViewById(R.id.walkingTime);
+                        TextView runningTime = (TextView) findViewById(R.id.runningTime);
+                        TextView date = (TextView) findViewById(R.id.currentDate);
+                        Log.d("TOUCHX",Float.toString(event.getX()));
+                        if (event.getX() <= 144.4) {
+                            date.setText("Sun., Dec 4th");
+                            runningTime.setText("00:00");
+                            bikingTime.setText("00:15");
+                            walkingTime.setText("01:00");
+                        } else if(event.getX() > 144.4 && event.getX() <= 216.5) {
+                            date.setText("Mon., Dec 7th");
+                            runningTime.setText("01:00");
+                            bikingTime.setText("00:25");
+                            walkingTime.setText("02:00");
+                        } else if(event.getX() > 216.5 && event.getX() <= 284.52) {
+                            date.setText("Tues., Dec 7th");
+                            runningTime.setText("00:00");
+                            bikingTime.setText("00:35");
+                            walkingTime.setText("01:00");
+                        }else if(event.getX() > 284.52 && event.getX() <= 353.6){
+                            date.setText("Wed., Dec 7th");
+                            runningTime.setText("00:45");
+                            bikingTime.setText("01:45");
+                            walkingTime.setText("00:30");
+                        } else if (event.getX() >= 353.6 && event.getX() <= 419.69) {
+                            date.setText("Thurs., Dec 9th");
+                            runningTime.setText("00:45");
+                            bikingTime.setText("00:35");
+                            walkingTime.setText("03:00");
+                        } else if (event.getX() >= 419.69 && event.getX() <= 487.79){
+                                date.setText("Fri., Dec 9th");
+                                runningTime.setText("01:25");
+                                bikingTime.setText("00:45");
+                                walkingTime.setText("02:00");
+                        } else {
+                            date.setText("Sat., Dec 10th");
+                            runningTime.setText("02:45");
+                            bikingTime.setText("00:15");
+                            walkingTime.setText("00:45");
+                        }
+                }
+
+                //Intent activityTracker = new Intent(BarGraph.this, ExerciseTracker.class);
                 //startActivity(activityTracker);
-                TextView bikingTime = (TextView) findViewById(R.id.bikingTime);
-                TextView walkingTime = (TextView) findViewById(R.id.walkingTime);
-                TextView runningTime = (TextView) findViewById(R.id.runningTime);
-                TextView date = (TextView) findViewById(R.id.currentDate);
 
+            return true;
 
-                date.setText("December 4th, 2016");
-                runningTime.setText("00:00");
-                bikingTime.setText("00:15");
-                walkingTime.setText("01:00");
             }
         });
 
@@ -167,104 +202,14 @@ public class BarGraph extends AppCompatActivity /*implements OnChartValueSelecte
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-   /* public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("BarGraph Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
-    @Override
-    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+    /*ensure that back after clicking a bar graph
+    will take you back to bargraph page*/
+    public void onBackPressed(){
+        super.onBackPressed();
+        startActivity(new Intent(BarGraph.this, BarGraph.class));
+        finish();
 
     }
-
-    @Override
-    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
-
-    }
-
-    @Override
-    public void onChartLongPressed(MotionEvent me) {
-
-    }
-
-    @Override
-    public void onChartDoubleTapped(MotionEvent me) {
-
-    }
-
-    @Override
-    public void onChartSingleTapped(MotionEvent me) {
-        //TODO: Make tapping bar take you to activity page FOR THAT DAY
-        SharedPreferences exerciseTracker = getSharedPreferences("exerciseTracker", Context.MODE_PRIVATE);
-        /*long walking = exerciseTracker.getInt("minutesWalking", 0);
-        long running = exerciseTracker.getInt("minutesRunning", 0);
-        long biking = exerciseTracker.getInt("minutesBiking", 0);*/
-      /*  setContentView(R.layout.activity_exercisetracker);
-        Intent activityTracker = new Intent(BarGraph.this, ExerciseTracker.class);
-        startActivity(activityTracker);
-        TextView bikingTime = (TextView) findViewById(R.id.bikingTime);
-        TextView walkingTime = (TextView) findViewById(R.id.walkingTime);
-        TextView runningTime = (TextView) findViewById(R.id.runningTime);
-        TextView date = (TextView) findViewById(R.id.currentDate);
-
-     //   Log.d("MEEEE", "BT "+bikingTime + " WT " +walkingTime);
-        //System.out.println(bikingTime);
-       /* int walking = activityTracker.getIntExtra("minutesWalking",0);
-        int running = activityTracker.getIntExtra("minutesRunning",0);
-        int biking = activityTracker.getIntExtra("minutesBiking",0);
-        //date.setText("December 4th, 2016");
-         // walkingTime.setText("05:35");
-  //      bikingTime.setText(Integer.toString(biking));
-   //     runningTime.setText(Integer.toString(running));
-
-
-    }
-
-    @Override
-    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
-
-    }
-
-    @Override
-    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
-
-    }
-
-    @Override
-    public void onChartTranslate(MotionEvent me, float dX, float dY) {
-
-    }*/
 
     /*This private class allows us to change the x-Axis labels to days
     * of the week instead of numbers*/
